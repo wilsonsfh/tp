8a13175676afde85e10b4a3e91dc23de7e2f7c31
WeiTianyue <e0968931@u.nus.edu>
2025-03-28 09:57:17 +0800

    Add more test cases for SDDCommand Parser

--- src/test/java/seedu/address/logic/parser/SetDueDateCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SetDueDateCommandParserTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DUE_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_INDEX;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,9 +13,6 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.SetDueDateCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
 
 /**
  * Test class for SetDueDateCommandParser.
@@ -29,7 +25,7 @@ public class SetDueDateCommandParserTest {
     public void parse_validInput_success() throws Exception {
         String userInput = "1 " + PREFIX_TASK_INDEX + "2 " + PREFIX_DUE_DATE + "2026-03-30 14:00";
         SetDueDateCommand expectedCommand = new SetDueDateCommand(
-                LocalDateTime.parse("2026-03-30 14:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                LocalDateTime.parse("2026-03-30T14:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME),
                 Index.fromOneBased(2),
                 Index.fromOneBased(1));
         assertEquals(expectedCommand, parser.parse(userInput));
@@ -56,6 +52,12 @@ public class SetDueDateCommandParserTest {
     @Test
     public void parse_pastDate_throwsParseException() {
         String userInput = "1 " + PREFIX_TASK_INDEX + "2 " + PREFIX_DUE_DATE + "2023-03-30 14:00";
+        assertThrows(ParseException.class, () -> parser.parse(userInput));
+    }
+
+    @Test
+    public void parse_incorrectPrefix_throwsParseException() {
+        String userInput = "1 " + "/taskint" + "2 " + PREFIX_DUE_DATE + "2026-03-30 14:00";
         assertThrows(ParseException.class, () -> parser.parse(userInput));
     }
 }
