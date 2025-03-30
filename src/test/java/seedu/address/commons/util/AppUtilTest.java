a67a7b01c94ecb5855e12060d347b39ad23c879a
wilsonsfh <wilsonsoon2010@hotmail.com>
2025-03-31 01:16:35 +0800

    Update AppUtilTest checkstyle

--- src/test/java/seedu/address/commons/util/AppUtilTest.java
+++ src/test/java/seedu/address/commons/util/AppUtilTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.MainApp;
 
 public class AppUtilTest {
 
